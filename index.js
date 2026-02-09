59360a3c5c2dc0243f9d99c819b267b9106eac78
Ankit raj <ankitrajbarhiya51@gmail.com>
2026-02-09 11:58:22 +0530

    backend part done completely

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+require("dotenv").config();
 const authRoutes = require("./routes/auth");
 
 const app = express();
@@ -9,7 +9,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/python_course");
+// mongoose.connect("mongodb://127.0.0.1:27017/python_course");
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
 
 app.use("/api/auth", authRoutes);
 
